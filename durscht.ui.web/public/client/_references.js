a8034f06b5631e92d61a22d334f4d746d17693ef
Andreas Danek <a.danek@live.com>
2015-04-08 12:35:15 +0200

    forgotten

--- durscht.ui.web/public/client/_references.js
+++ durscht.ui.web/public/client/_references.js
@@ -1,2 +1,3 @@
 /// <reference path="./lib/typings/angular/angular.d.ts"/>
+/// <reference path="./lib/typings/jquery/jquery.d.ts"/>
 /// <reference path="./services/barService.ts"/>
---
+++ durscht.ui.web/public/client/services/PostingDataService.js
@@ -0,0 +1,16 @@
+/// <reference path="../_references.ts"/>
+(function (app) {
+    app.service("posting", function () {
+        var posting = this;
+        posting.bar = { id: -1, name: "", distance: "0.0 km", beers: [] };
+        posting.beer = { brand: "", type: "", description: "" };
+        posting.remark = "";
+        posting.price = 0.0;
+        posting.reset = function () {
+            posting.bar = { id: -1, name: "", distance: "0.0 km", beers: [] };
+            posting.beer = { brand: "", type: "", description: "" };
+            posting.remark = "";
+            posting.price = 0.0;
+        };
+    });
+})(angular.module('durscht-core'));
