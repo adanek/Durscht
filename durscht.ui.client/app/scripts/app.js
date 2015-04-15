95256c0ab1b623db9aacc57872e78c674558114c
unknown <adanek@Lenovo-PC.home.local>
2015-04-15 09:03:05 +0200

    Client project created
    Signed-off-by: unknown <adanek@Lenovo-PC.home.local>

---
+++ durscht.ui.client/app/scripts/app.js
@@ -0,0 +1,33 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name durschtApp
+ * @description
+ * # durschtApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('durschtApp', [
+    'ngAnimate',
+    'ngCookies',
+    'ngResource',
+    'ngRoute',
+    'ngSanitize',
+    'ngTouch'
+  ])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl'
+      })
+      .when('/about', {
+        templateUrl: 'views/about.html',
+        controller: 'AboutCtrl'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
---
+++ durscht.ui.client/app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name durschtApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the durschtApp
+ */
+angular.module('durschtApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
