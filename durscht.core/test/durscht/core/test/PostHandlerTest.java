19fd3f0e6852850abeb3aa3ef8476a5543662924
MartinNoc <martin.nocker@student.uibk.ac.at>
2015-04-17 18:36:01 +0200

    extended PostHandler Unit Test

--- durscht.core/test/durscht/core/test/PostHandlerTest.java
+++ durscht.core/test/durscht/core/test/PostHandlerTest.java
@@ -2,14 +2,20 @@ package durscht.core.test;
 
 import static org.junit.Assert.assertEquals;
 
+import java.util.ArrayList;
+import java.util.Collection;
 import java.util.NoSuchElementException;
 
 import org.junit.Before;
 import org.junit.Test;
 import org.mockito.Mockito;
 
+import durscht.contracts.data.IBar;
+import durscht.contracts.data.IBeer;
 import durscht.contracts.data.IDataHandler;
 import durscht.core.PostHandler;
+import durscht.model.Bar;
+import durscht.model.Beer;
 
 public class PostHandlerTest {
 
@@ -37,16 +43,39 @@ public class PostHandlerTest {
 	}
 	
 	@Test
+	public void getNearBars() {
+		Collection<IBar> bars = new ArrayList<IBar>();
+		Collection<IBeer> beers = new ArrayList<IBeer>();
+		Bar bar = new Bar();
+		bar.setId(26);
+		bars.add(bar);
+		
+		Beer beer = new Beer();
+		beers.add(beer);
+		
+		IDataHandler dataHandler = Mockito.mock(IDataHandler.class);
+		Mockito.when(dataHandler.getBarsCoordinates(Mockito.anyDouble(),Mockito.anyDouble(),
+				Mockito.anyDouble(),Mockito.anyDouble())).thenReturn(bars);
+		Mockito.when(dataHandler.getAllBeersFromBar(26)).thenReturn(beers);
+		postHandler.setDataHandler(dataHandler);
+		
+		int expected = 26;
+		int actual = postHandler.getNearBars(47, 11)[0].getId();
+		
+		assertEquals(expected, actual);
+	}
+	
+	@Test
 	public void putPostingTest() {
 		
 		IDataHandler dataHandler = Mockito.mock(IDataHandler.class);
 		Mockito.when(dataHandler.createPost(0, 0, 0, "test")).thenReturn(26);
 		postHandler.setDataHandler(dataHandler);
 		
-		int want = 26;
-		int have = postHandler.putPosting(0, 0, 0, "test");
+		int expected = 26;
+		int actual = postHandler.putPosting(0, 0, 0, "test");
 		
-		assertEquals(want,have);
+		assertEquals(expected,actual);
 	}
 
 }
