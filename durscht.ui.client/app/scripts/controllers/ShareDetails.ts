b0481fd76b2fb2fccc499fca3fd5c54575e898b7
adanek <andreas.danek-bulius@student.uibk.ac.at>
2015-04-21 00:13:14 +0200

    ShareDetails moved to client

--- durscht.ui.client/app/scripts/controllers/ShareDetails.ts
+++ durscht.ui.client/app/scripts/controllers/ShareDetails.ts
@@ -1,10 +1,15 @@
 /// <reference path="../_references.ts"/>
 
 (function (app){
-    var controller = function ($scope, posting:Posting){
+    var controller = function ($scope, $location, posting:Posting){
         $scope.caption = "Aha!";
         $scope.remark = "";
         $scope.price = 0.0;
+
+
+        $scope.sentPost = function(){
+            $location.path("/").replace();
+        }
     };
 
     app.controller("ShareDetailsCtrl", controller);
