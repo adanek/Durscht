a1351422aecb0a3b20f3fb5a401e2edc422dccf8
Pati <patrick.deutsch@outlook.com>
2015-04-26 15:26:09 +0200

    TestDataCorrection

--- durscht.data/src/DatabaseUpdateTest.java
+++ durscht.data/src/DatabaseUpdateTest.java
@@ -30,22 +30,22 @@ public class DatabaseUpdateTest {
 			try{
 				//get handler
 				IDataHandler handler  = new DataHandler();
-				
+				/*
 				//create new users
 				int userid1 = handler.createUser("admin", "admin@gmx.at", "admin");
 				int userid2 = handler.createUser("user2", "user2@gmx.at", "user2");
 				int userid3 = handler.createUser("user3", "user3@gmx.at", "user3");
 	
 				//create new beers
-				int beerid1 = handler.createBeer("Gösser", "Herbes österreichisches Bier");
+				int beerid1 = handler.createBeer("GÃ¶sser", "Herbes Ã¶sterreichisches Bier");
 				int beerid2 = handler.createBeer("Stiegl", "Originales traditionelles Salzburger Bier");
 				int beerid3 = handler.createBeer("Ottakringer", "Traditionelles Wiener Bier");
 				int beerid4 = handler.createBeer("Starkenberger", "Heimisches Bier aus dem Schloss Starkenberg in Tarrenz");
 				
 				//create new Bars
-				int barid1 = handler.createBar("Theresien Bräu", 47.263835, 11.394931, "Traditionelles Bierbrauerei aus Innsbruck, das ein sehr gutes Restaurant ist und auch zum Ausgehen gut geeignet ist.", "www.brauwirtshaus.at");
-				int barid2 = handler.createBar("11er Haus", 47.268653, 11.392825, "Sehr bekannte Bar in Innsbruck, die sehr viele Biersorten der ganzen Welt anbietet.", "http://innsbruckplus.at/elferhaus/");
-				int barid3 = handler.createBar("Stadtcafe", 47.268785, 11.395963, "Bekanntestes Nachtlokal in Innsbruck, das hauptsächlich von der Studentenszene besucht wird.", "www.tagnacht.at/stadtcafe/");
+				int barid1 = handler.createBar("Patis Testbar", 11.4040792, 47.269258, "geile Cocktails!!!", "Test");
+				int barid2 = handler.createBar("11er Haus", 11.392825, 47.268653, "Sehr bekannte Bar in Innsbruck, die sehr viele Biersorten der ganzen Welt anbietet.", "http://innsbruckplus.at/elferhaus/");
+				int barid3 = handler.createBar("Stadtcafe", 11.395963, 47.268785, "Bekanntestes Nachtlokal in Innsbruck, das hauptsÃ¤chlich von der Studentenszene besucht wird.", "www.tagnacht.at/stadtcafe/");
 				
 				//create new achievement
 				int achid1 = handler.createAchievement("10 posts", "The user will get this achievement when he drinks 10 beers and add a post to the app");
@@ -57,7 +57,7 @@ public class DatabaseUpdateTest {
 				int post3 = handler.createPost(barid2, beerid3, userid1, 3.0, 2, "Post3");
 				int post4 = handler.createPost(barid2, beerid4, userid3, 3.0, 2, "Post4");
 				
-				/*IUser user = handler.getUserLogin("user2", "user2");
+				IUser user = handler.getUserLogin("user2", "user2");
 				
 				Collection<IBeer> beers = handler.getAllBeers();
 				Collection<IBeer> bars1 = handler.getAllBeersFromBar(barid1);
@@ -66,8 +66,10 @@ public class DatabaseUpdateTest {
 				Collection<IBeerPost> posts1 = handler.getAllPostsFromBar(barid1);
 				Collection<IBeerPost> posts2 = handler.getAllPostsFromBar(barid2);
 				Collection<IBeerPost> userposts1 = handler.getAllPostsFromUser(userid1);
-				Collection<IBeerPost> userposts2 = handler.getAllPostsFromUser(userid2);*/
+				Collection<IBeerPost> userposts2 = handler.getAllPostsFromUser(userid2);
 					
+				Collection<IBar> bars = handler.getBarsCoordinates(11.4040792, 11.4040792, 47.269258, 47.269258);
+				*/
 				handler.closeDatabaseConnection();
 			}catch(Exception e){}
 	}
