ff97a7c3c5371f713a5324348067436992c86d91
adanek <andreas.danek-bulius@student.uibk.ac.at>
2015-05-24 12:46:45 +0200

    design

--- durscht.ui.client/app/scripts/controllers/Main.ts
+++ durscht.ui.client/app/scripts/controllers/Main.ts
@@ -39,6 +39,34 @@
            console.info("Now");
             adjustBodySize();
         });
+
+        function adjustBodySize() {
+
+            var wh = $(window).height();
+            var fh = $('body > footer').outerHeight(true);
+            var hh = $('body > header').height();
+
+            // Reset heights
+            $('body').height('auto');
+            $('body > article').height('auto');
+            $('body > article > header').css({lineHeight: 'normal'});
+            $('body > article > section').css({lineHeight: 'normal'});
+
+            var bh = $('body').height();
+            var ah = $('body > article').height();
+
+            var bh = (wh - fh) > bh ? (wh - fh) : bh;
+            var ah = (wh - hh - fh) > ah ? (wh - hh - fh) : ah;
+            var af = $('article > footer').outerHeight(true);
+            af = af > 0 ? af : 0;
+
+            $('body').height(bh);
+            $('body > article').height(ah);
+            $('body > article > header').css({lineHeight: (Math.round(20 * ah / 100))+'px'});
+            $('body > article > header').css({lineHeight: (Math.round(20 * ah / 100))+'px'});
+            $('body > article > section').css({lineHeight: (Math.round(80 * ah / 100) - af)+'px'});
+
+        }
     };
 
     app.controller("MainCtrl", ['$scope', 'authService', '$location', controller]);
