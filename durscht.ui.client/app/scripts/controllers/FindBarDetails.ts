65f2d0940f9811a70f1f7abb030227627681e8e3
adanek <andreas.danek-bulius@student.uibk.ac.at>
2015-06-01 14:41:28 +0200

    BUGFIX https

--- durscht.ui.client/app/scripts/controllers/FindBarDetails.ts
+++ durscht.ui.client/app/scripts/controllers/FindBarDetails.ts
@@ -8,17 +8,11 @@
         var caption:string ;
         var bar:Bar;
 
-
         var barId = searchService.barId;
         barService.getBarDetails(barId).success(function (data) {
             bar = data;
             $scope.caption = bar.name;
         });
-
-
-
-
-
     }
 
     app.controller('FindBarDetailsCtrl', ['$scope', '$location', 'barService', 'searchService', ctrl]);
